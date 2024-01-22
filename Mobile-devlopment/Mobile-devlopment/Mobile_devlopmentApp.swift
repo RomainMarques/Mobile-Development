12ed1bc9bafa4e0ac4388664044a000fe08abe20
RomainMarques <77971683+RomainMarques@users.noreply.github.com>
2024-01-22 14:15:37 +0100

    project created

---
+++ Mobile-devlopment/Mobile-devlopment/Mobile_devlopmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mobile_devlopmentApp.swift
+//  Mobile-devlopment
+//
+//  Created by Romain Marques on 22/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct Mobile_devlopmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

4e5bf335217cd1b48e91e172fa5934938d1431e0
RomainMarques <77971683+RomainMarques@users.noreply.github.com>
2024-01-22 15:10:45 +0100

    creating activityview

---
+++ Mobile-devlopment/Mobile-devlopment/View/ActivityView.swift
@@ -0,0 +1,25 @@
+//
+//  ActivityView.swift
+//  Mobile-devlopment
+//
+//  Created by Romain Marques on 22/01/2024.
+//
+
+import Foundation
+import SwiftUI
+
+struct ActivityView: View {
+    var activity: Activity
+    var body: some View {
+        VStack {
+            
+            Text("Schedule")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
+

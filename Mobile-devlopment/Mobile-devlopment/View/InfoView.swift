440f6baa008b5785bea082f75476e8b0d46fd309
Fefoler01 <72406859+Fefoler01@users.noreply.github.com>
2024-01-29 16:07:27 +0100

    Add infoView, some style

---
+++ Mobile-devlopment/Mobile-devlopment/View/InfoView.swift
@@ -0,0 +1,37 @@
+//
+//  InfoView.swift
+//  Mobile-devlopment
+//
+//  Created by goldorak on 29/01/2024.
+//
+
+import SwiftUI
+
+struct InfoView: View {
+    var title: String
+    var value: String
+    
+    var body: some View {
+        HStack {
+            Text("\(title):")
+                .font(.headline)
+                .foregroundColor(.black)
+                .bold()
+            
+            Text(value)
+                .font(.subheadline)
+                .foregroundColor(.blue)
+        }
+        .padding(.vertical, 5)
+    }
+}
+
+/*struct InfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        InfoView()
+    }
+}*/
+
+#Preview {
+    InfoView(title: "Test", value: "test")
+}
